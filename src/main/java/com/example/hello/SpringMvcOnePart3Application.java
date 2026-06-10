522fc09b678f7684a004117d5cca27e09eeb2251
dyddlfsla <7942dyddlfsla@gmail.com>
2026-06-10 19:54:28 +0900

    initial commit

---
+++ src/main/java/com/example/hello/SpringMvcOnePart3Application.java
@@ -0,0 +1,13 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcOnePart3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcOnePart3Application.class, args);
+	}
+
+}
