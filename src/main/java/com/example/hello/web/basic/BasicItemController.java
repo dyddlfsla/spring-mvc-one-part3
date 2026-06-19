1f0c25576b22cab6d5f52d1a540376525229faff
dyddlfsla <7942dyddlfsla@gmail.com>
2026-06-19 23:12:58 +0900

    feat: save item

--- src/main/java/com/example/hello/web/basic/BasicItemController.java
+++ src/main/java/com/example/hello/web/basic/BasicItemController.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -32,7 +34,17 @@ public class BasicItemController {
     return "basic/item";
   }
 
+  @GetMapping("/add")
+  public String addForm() {
+    return "basic/addForm";
+  }
 
+  @PostMapping("/add")
+  public String save(@ModelAttribute Item item) {
+    itemRepository.save(item);
+
+    return "basic/item";
+  }
 
   @PostConstruct
   public void init() {
